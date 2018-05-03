1e610030e89222ec75af8680120213a4334e6f46
Andrés Felipe Aguilar Rendón <afaguilarr@unal.edu.co>
2018-05-02 22:04:35 -0500

    Module 2 Use cases completed

--- src/app/general-service.service.ts
+++ src/app/general-service.service.ts
@@ -5,11 +5,16 @@ import { Company } from './shared/company';
 @Injectable()
 export class GeneralServiceService {
   user_type;
+  username;
   users = [new User("Andres Felipe Aguilar","afaguilarr","ElMejor123","Developer"),
     new User("John Jairo Serna","jjsernaco","holaMUNDO456","Project Manager"),
-    new User("Carlos Mario Zapata","cmzapata","EnserioEsaEsTuPregunta?","Game Administrator")];
+    new User("Carlos Mario Zapata","cmzapata","EnserioEsaEsTuPregunta?","Game Administrator"),
+    new User("David","dddavid","david","Project Manager")
+  ];
   user_to_be_updated = this.users[1];
-  companies = [];
-  company_to_be_updated;
+  companies = [new Company("UNAL","jjsernaco",
+    "http://unal.edu.co/typo3conf/ext/unal_skin_default/Resources/Public/images/escudoUnal_black.png"),
+  new Company("Google",undefined,"http://educainternet.es/pictures/4074.png")];
+  company_to_be_updated = this.companies[1];
   constructor() { }
 }
--- src/app/update-company/update-company.component.ts
+++ src/app/update-company/update-company.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GeneralServiceService } from '../general-service.service';
 import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Company} from "../shared/company";
 
 @Component({
   selector: 'app-update-company',
@@ -11,10 +13,113 @@ export class UpdateCompanyComponent implements OnInit {
 
   constructor(public service: GeneralServiceService, public router: Router) { }
 
-  ngOnInit() {
-    if (this.service.user_type === undefined) {
-      this.router.navigate([''])
+  form(){
+    this.formdata = new FormGroup({
+      name: new FormControl(''),
+      img: new FormControl(''),
+      project_manager: new FormControl('')
+    });
+  }
+
+  possible_project_managers(){
+    this.project_managers = []
+    for (let user of this.service.users){
+      if (user.role === "Project Manager"){
+        this.project_managers.push(user);
+        for(let company of this.service.companies){
+          if(company.project_manager_username === user.username){
+            this.project_managers.pop();
+            break;
+          }
+        }
+      }
     }
+  }
+
+  new_name(name){
+    for(let company of this.service.companies){
+      if(name === company.name){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  current_project_manager(){
+    if(this.service.company_to_be_updated.project_manager_username === undefined){
+      this.lacking_project_manager = true;
+    }
+    else{
+      this.lacking_project_manager = false;
+    }
+  }
+
+  formdata;
+  project_managers;
+  lacking_project_manager = false;
+  totally_empty = false;
+  invalid = false;
+  success = false;
+  repeated_field = false;
+  hide = true;
+  user;
+
+  ngOnInit() {
+    /*if (this.service.user_type === undefined) {
+       this.router.navigate([''])
+     }
+
+    else if (this.service.user_type === "Team Member") {
+       this.router.navigate(['restricted'])
+     }
+
+     else {*/
+    this.current_project_manager();
+    this.possible_project_managers();
+    this.form();
+    //}
+  }
+
+  onClickSubmit(data) {
+    if(data.name === '' && data.img === '' &&
+      (!(this.lacking_project_manager) || data.project_manager === '' || data.project_manager === undefined)){
+      this.totally_empty = true;
+      this.invalid = false;
+      this.success = false;
+      this.repeated_field = false;
+    }
+    else if(!(this.new_name(data.name))){
+      this.totally_empty = false;
+      this.invalid = true;
+      this.success = false;
+      this.repeated_field = false;
+    }
+    else if(data.name === this.service.company_to_be_updated.name || data.img === this.service.company_to_be_updated.image){
+      this.totally_empty = false;
+      this.invalid = false;
+      this.success = false;
+      this.repeated_field = true;
+    }
+    else if(this.new_name(data.name)){
+      if(!(data.name === '')){
+        this.service.company_to_be_updated.name = data.name;
+      }
+      if(!(data.img === '')){
+        this.service.company_to_be_updated.image = data.img;
+      }
+      if(!(data.project_manager === '' || data.project_manager === undefined || !(this.lacking_project_manager))){
+        this.service.company_to_be_updated.project_manager_username = data.project_manager;
+      }
+      this.totally_empty = false;
+      this.invalid = false;
+      this.success = true;
+      this.repeated_field = false;
+      this.possible_project_managers();
+      this.current_project_manager();
+      console.log(this.service.companies);
+      this.form();
+    }
+
   }
 
 }
