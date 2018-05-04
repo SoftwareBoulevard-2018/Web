d73d2fc8526051572ca88a1d817b1080f8052931
Víctor Daniel Jaramillo Gómez <edie5283@gmail.com>
2018-05-03 21:24:06 -0500

    .

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import { EmailComponent } from './email/email.component';
 if (environment.production) {
   enableProdMode();
 }
