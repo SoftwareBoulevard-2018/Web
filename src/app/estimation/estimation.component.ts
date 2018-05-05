265210ded478914541bf5397e5dc25c2e73f2ff2
Juan Chaves <jpchavesm@unal.edu.co>
2018-05-05 03:10:03 -0500

    Estimation logic (in progress)

--- src/app/estimation/estimation.component.ts
+++ src/app/estimation/estimation.component.ts
@@ -3,6 +3,7 @@ import {GeneralServiceService} from '../general-service.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {Company} from '../shared/company';
+import {BiddingProject} from '../shared/biddingProject';
 
 @Component({
   selector: 'app-estimation',
@@ -11,9 +12,11 @@ import {Company} from '../shared/company';
 })
 export class EstimationComponent implements OnInit {
   guess;
-  constructor(public service: GeneralServiceService, public router: Router) { }
 
-  form(){
+  constructor(public service: GeneralServiceService, public router: Router) {
+  }
+
+  form() {
     this.guess = new FormGroup({
       cost: new FormControl('',
         Validators.compose([
@@ -26,31 +29,39 @@ export class EstimationComponent implements OnInit {
     });
   }
 
-  estimation_validation(guess){
-    for(let company of this.service.companies){
-      if(name === company.name){
-        return false;
+  estimation_validation(guess) {
+    for (let company of this.service.companies) {
+      if (this.service.username === company.project_manager_username) {
+        for (let project of this.service.projects) {
+          if (company.active_project === project.project_id) {
+            var current_project = project;
+          }
+        }
       }
     }
-    return true;
-  }
+    const max_time = current_project.time + current_project.time * 0.1;
+    const min_time = current_project.time - current_project.time * 0.1;
+    const max_cost = current_project.cost + current_project.cost * 0.1;
+    const min_cost = current_project.cost - current_project.cost * 0.1;
 
-  onClickSubmit(data) {
-    if(this.new_name(data.name)){
-      this.service.companies.push(new Company(data.name,data.project_manager,data.img))
-      this.invalid = false;
-      this.success = true;
-      this.possible_project_managers();
-      this.form();
-    }
-    else{
-      this.invalid = true;
-      this.success = false;
-    }
+    return (guess.time >= min_time && guess.time <= max_time) && (guess.cost >= min_cost && guess.cost <= max_cost);
   }
 
   ngOnInit() {
-
+    // if (this.service.user_type === undefined) {
+    //  this.router.navigate(['']);
+    // }
+    if (this.service.user_type === "Team Member") {
+      this.router.navigate(['restricted']);
+    }
+    else {
+      this.form();
+    }
   }
 
+  onClickSubmit(guess) {
+    if(this.estimation_validation(guess)){
+      this.router.navigate(['home']);
+    }
+  }
 }
--- src/app/shared/instantProject.ts
+++ src/app/shared/instantProject.ts
@@ -2,9 +2,9 @@ export class InstantProject {
   project_id: number;
   project_name: string;
   rewarded_k: number;
-  constructor(project_id?: number, project_name?: string, rewarded_k?: number){
+  constructor(project_id?: number, project_name?: string, rewarded_k?: number) {
     this.project_id = project_id;
-    this.project_name = name;
+    this.project_name = project_name;
     this.rewarded_k = rewarded_k;
   }
 }
