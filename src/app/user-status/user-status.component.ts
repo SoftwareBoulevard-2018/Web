7178824c63450cfc29f825fdea760fafb0304ea7
Andrés Felipe Aguilar Rendón <afaguilarr@unal.edu.co>
2018-05-06 17:41:04 -0500

    User-status completed, Companies table modified

--- src/app/user-status/user-status.component.ts
+++ src/app/user-status/user-status.component.ts
@@ -10,6 +10,9 @@ import {Router} from '@angular/router';
 export class UserStatusComponent implements OnInit {
 
   current_company;
+  performance: number;
+  color = 'primary';
+  mode = 'determinate';
 
   constructor(public service: GeneralServiceService, public router: Router) { }
 
@@ -18,9 +21,19 @@ export class UserStatusComponent implements OnInit {
       this.router.navigate(['']);
     }
     this.current_company = this.search_company(this.service.user_to_be_updated.company_name);
+    if ((this.service.user_to_be_updated.questions_answered_wrong + this.service.user_to_be_updated.questions_answered_right) !== 0) {
+      this.performance = (this.service.user_to_be_updated.questions_answered_right /
+        (this.service.user_to_be_updated.questions_answered_wrong + this.service.user_to_be_updated.questions_answered_right)) * 100;
+    } else {
+      this.performance = 0;
+    }
   }
   redirect(event) {
     this.router.navigate(['home/users/user-status/update']);
+  }
+  redirect2(event) {
+    this.service.company_to_be_updated = this.current_company;
+    this.router.navigate(['home/companies/company-status']);
   }
   date_formatter(date?: Date) {
     return date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate();
