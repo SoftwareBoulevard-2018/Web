383f419bcd95cceb63223cc45200a2d652ef5fa0
Juan Chaves <jpchavesm@unal.edu.co>
2018-05-07 00:07:29 -0500

    Estimation component working
    Se corrigió el código del componente de estimación. Por ahora este componente revisa si el user es un project manager, y si su compañía tiene al menos un recurso.
    
    Cada intento de estimación, exitoso o no, gasta un recurso de la compañía.

--- src/app/estimation/estimation.component.ts
+++ src/app/estimation/estimation.component.ts
@@ -11,11 +11,11 @@ import {BiddingProject} from '../shared/biddingProject';
   styleUrls: ['./estimation.component.css']
 })
 export class EstimationComponent implements OnInit {
-  // guess;
+  guess;
 
   constructor(public service: GeneralServiceService, public router: Router) {
   }
-  /*
+
   form() {
     this.guess = new FormGroup({
       cost: new FormControl('',
@@ -29,39 +29,74 @@ export class EstimationComponent implements OnInit {
     });
   }
 
-  estimation_validation(guess) {
-    for (let company of this.service.companies) {
-      if (this.service.username === company.project_manager.username) {
-        for (let project of this.service.projects) {
-          if (company.active_project === project.project_id) {
-            var current_project = project;
+  getProject(username){
+    for (let user of this.service.users) {
+      if (username === user.username) {
+        for (let company of this.service.companies) {
+          if (user.company_name === company.name) {
+            for(let project of this.service.projects){
+              if(company.current_project_name === project.project_name) {
+                return project;
+              }
+            }
           }
         }
       }
     }
+  }
+
+  estimation_validation(guess) {
+
+    const current_project = this.getProject(this.service.username);
+
     const max_time = current_project.time + current_project.time * 0.1;
     const min_time = current_project.time - current_project.time * 0.1;
     const max_cost = current_project.cost + current_project.cost * 0.1;
     const min_cost = current_project.cost - current_project.cost * 0.1;
 
     return (guess.time >= min_time && guess.time <= max_time) && (guess.cost >= min_cost && guess.cost <= max_cost);
-  }*/
-
-  ngOnInit() {
-    /*if (this.service.user_type === undefined) {
-     this.router.navigate(['']);
-    }
-    else if (this.service.user_type === "Team Member") {
-      this.router.navigate(['restricted']);
-    }
-    else {
-      this.form();
-    }*/
   }
 
-  /*onClickSubmit(guess) {
-    if(this.estimation_validation(guess)){
-      this.router.navigate(['home']);
+  getCompany(username){
+    for (let user of this.service.users) {
+      if (username === user.username) {
+        for (let company of this.service.companies) {
+          if (user.company_name === company.name) {
+            return company;
+          }
+        }
+      }
     }
-  }*/
+  }
+
+  ngOnInit() {
+    if (this.service.user_type === undefined) {
+     this.router.navigate(['']);
+    } else if (this.service.user_type === "Team Member") {
+      this.router.navigate(['restricted']);
+    } else {
+      let userCompany = this.getCompany(this.service.username);
+      if(userCompany.resources >= 1) {
+        this.form();
+      }
+      else {
+        // TODO : POPUP WARNING ABOUT LACK OF RESOURCES
+      }
+    }
+  }
+
+  onClickSubmit(guess) {
+    let userCompany = this.getCompany(this.service.username);
+    userCompany.resources -= 1;
+    if (this.estimation_validation(guess)) {
+      this.router.navigate(['home/users/projectmanager/functions']);
+      // TODO: POPUP CONGRATULATING THE USER
+    } else {
+      // TODO: POPUP WARNING THE USER
+    }
+  }
+
+  redirecttopm(event) {
+    this.router.navigate(['home/users/projectmanager/functions']);
+  }
 }
