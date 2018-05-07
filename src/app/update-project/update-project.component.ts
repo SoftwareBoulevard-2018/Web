6aaed8e2886aa909fd8f020504b369346b36c4e1
Angel David Machado <amachadoa@unal.edu.co>
2018-05-07 11:59:23 -0500

    update project

--- src/app/update-project/update-project.component.ts
+++ src/app/update-project/update-project.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GeneralServiceService } from '../general-service.service';
+import {Router} from "@angular/router";
+import {EmailComponent} from "../email/email.component";
+import {MatTableDataSource, MatPaginator, MatSort} from "@angular/material";
 
 @Component({
   selector: 'app-update-project',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: GeneralServiceService, public router: Router) { }
+
+  projects = [];
+  project2;
+
+  table_titles = ["id", "name", "update"];
 
   ngOnInit() {
+    console.log(this.service.user_type);
+    if (this.service.user_type === undefined) {
+      this.router.navigate([''])
+    }
+
+    else if (this.service.user_type === "Team Member" || this.service.user_type === "Project Manager") {
+      this.router.navigate(['restricted'])
+    }
+
+    else {
+      this.projects = JSON.parse(JSON.stringify(this.service.projects));
+      this.project2 = new MatTableDataSource(this.projects);
+      //this.users2.paginator = this.paginator;
+      //this.users2.sort = this.sort;
+    }
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.project2.filter = filterValue;
+  }
+
+  search_project(name) {
+    for (let project of this.service.companies) {
+      if (company.name === name) {
+        return company;
+      }
+    }
   }
 
 }
