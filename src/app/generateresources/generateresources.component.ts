5a76cc8a3f569889e68f7e94f834d4c468778364
Juan Chaves <jpchavesm@unal.edu.co>
2018-05-07 23:53:43 -0500

    Generate resources a medias

--- src/app/generateresources/generateresources.component.ts
+++ src/app/generateresources/generateresources.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {BiddingProject} from '../shared/biddingProject';
+import {GeneralServiceService} from '../general-service.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-generateresources',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateresourcesComponent implements OnInit {
 
-  constructor() { }
   solved_puzzle = false;
+  redirect1($event) {
+    this.solved_puzzle = true;
+  }
+  constructor(public service: GeneralServiceService, public router: Router) {
+  }
+
+  redirectToFunctions(event) {
+    this.router.navigate(['home/users/projectmanager/functions']);
+  }
+
   ngOnInit() {
+    if (this.service.user_type === undefined) {
+      this.router.navigate(['']);
+    }
+    else if (this.service.user_type === "Team Member") {
+      this.router.navigate(['restricted']);
+    }
   }
 
 }
+
+
