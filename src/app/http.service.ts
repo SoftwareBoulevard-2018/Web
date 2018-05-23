3d2ad7bc2588917a3fb7740aa16d4dba17ec31b5
vdjaramillo <33434357+vdjaramillo@users.noreply.github.com>
2018-05-23 17:02:02 -0500

    Update http.service.ts

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -19,7 +19,7 @@ export class HttpService {
 
   constructor(public http: HttpClient) { }
 
-  static apiURL = 'http://localhost:3000';
+  static apiURL = 'http://35.196.111.251:3000';
   static usersURL = '/users';
   static usersURL2 = '/username';
   static companiesURL = '/companies';
