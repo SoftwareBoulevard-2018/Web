8e0b40d5e574928dfef8de6d2d25fbff5ef5d113
Jose Posada <josedanielpm@gmail.com>
2018-05-25 23:26:47 -0500

    Revert "Merge branch 'master' of https://github.com/SoftwareBoulevard-2018/Web"
    This reverts commit 6b7aa05ccebba9678412c340524d5bf75180a561, reversing
    changes made to af3420ff48e96a87e26b71bfe050ee091061eab7.

--- src/app/shared/email.ts
+++ src/app/shared/email.ts
@@ -1,12 +1,17 @@
 export class Email{
+	id: string;
 	sender: string;
-	subject: string
 	receivers: [string];
 	content: string;
-	constructor(sender?:  string, subject?: string, receivers?: [string], content?: string){
+	createdAt: string;
+	acknowledgment: [string];
+	constructor( id?: string, sender?:  string, receivers?: [string], content?: string, state?: string, 
+		createdAt?: string, acknowledgment?: [string]){
+		this.id = id;
 		this.receivers = receivers;
-		this.subject = subject;
 		this.sender = sender;
 		this.content = content;
+		this.createdAt = createdAt;
+		this.acknowledgment = acknowledgment;
 	}
 }
