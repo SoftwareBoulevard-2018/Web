ebd8166922c0bb69061f3ddb7579e9ed6514fd06
Juan David Fernández Moreno <judfernandezmo@unal.edu.co>
2018-05-29 15:12:12 -0500

    formas de solucionar puzzle

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -25,8 +25,8 @@ export class HttpService {
 
   constructor(public http: HttpClient) { }
 
-  static apiURL = 'http://35.196.111.251:3000';
-  //static apiURL = 'http://localhost:3000';
+  //static apiURL = 'http://35.196.111.251:3000';
+  static apiURL = 'http://localhost:3000';
   static usersURL = '/users';
   static usersURL2 = '/username';
   static usersURL3 = '/usersByRole';
