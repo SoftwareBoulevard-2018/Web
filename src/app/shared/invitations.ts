8751ac6db27740867d90f1f1e5426a9c1f51add1
dacalles <dacalles@unal.edu.co>
2018-05-30 18:24:32 -0500

    Invitations full

---
+++ src/app/shared/invitations.ts
@@ -0,0 +1,11 @@
+export class invitations{
+	user: string;
+	company: string;
+	state: string;
+  constructor(user?: string, company?: string, state?: string,){
+              this.user=user;
+              this.company=company;
+              this.state=state;
+
+}
+}
