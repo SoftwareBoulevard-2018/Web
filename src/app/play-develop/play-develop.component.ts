5ec135618c365313334a65e39717d36d6262abf6
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2018-05-31 00:50:03 -0500

    boton elimina equipo

--- src/app/play-develop/play-develop.component.ts
+++ src/app/play-develop/play-develop.component.ts
@@ -4,6 +4,7 @@ import {Router} from "@angular/router";
 import {EmailComponent} from "../email/email.component";
 import {MatTableDataSource, MatPaginator, MatSort} from "@angular/material";
 import {Answer} from '../shared/answer';
+import {HttpService} from '../http.service';
 
 @Component({
   selector: 'app-play-develop',
@@ -14,7 +15,7 @@ export class PlayDevelopComponent implements OnInit {
   correct ;
   control: Answer;
   quest ;
-  constructor(public service: GeneralServiceService, public router: Router) { }
+  constructor(public httpService: HttpService, public service: GeneralServiceService, public router: Router) { }
 
   ngOnInit() {
     console.log(this.service.user_type);
@@ -25,6 +26,10 @@ export class PlayDevelopComponent implements OnInit {
     this.correct = obtain_correct(this.service.questions[0]);
   }
 
+  redirect1(event){
+    this.service.user.companyId = null;
+    this.httpService.updateUser(this.service.user, this.service.user.id);
+  }
 
 }
 function  obtain_correct(Quest_n) {
@@ -33,4 +38,5 @@ function  obtain_correct(Quest_n) {
       return correct;
     }
   }
+
 }
