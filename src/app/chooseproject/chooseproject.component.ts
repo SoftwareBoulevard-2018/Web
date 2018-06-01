6ff34e32c484717c238634c87f16dfdd1555197d
dacalles <dacalles@unal.edu.co>
2018-05-31 23:01:52 -0500

    chooseproject correction

--- src/app/chooseproject/chooseproject.component.ts
+++ src/app/chooseproject/chooseproject.component.ts
@@ -133,7 +133,7 @@ export class ChooseprojectComponent implements OnInit {
   }
 
   getProject(formdata) {
-    this.record = new Record(null, this.service.user.companyId, formdata.id);
+    this.record = new Record(null,null, this.service.user.companyId, formdata.id);
     console.log(this.record);
     console.log("__________________________________________");
     console.log(this.httpService.createRecord(this.record));
