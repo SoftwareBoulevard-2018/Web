71ba6d9e6b8f0e7eeb7605d06537d20416761613
Daniel Velez Santamaria <danvelezsan@unal.edu.co>
2018-06-01 22:15:12 -0500

    fail

--- src/app/create-certification/create-certification.component.ts
+++ src/app/create-certification/create-certification.component.ts
@@ -12,21 +12,18 @@ export class CreateCertificationComponent implements OnInit {
   constructor(public service: GeneralServiceService, public router: Router) { }
 
   redirect1(event) {
-	// Redirects to New Bidding project
     if(this.service.user_type === "Game Administrator"){
       this.router.navigate([]);
     }
   }
 
   redirect2(event) {
-	// Redirects to New Instant project project
     if(this.service.user_type === "Game Administrator"){
       this.router.navigate([]);
     }
   }
 
   redirect3(event) {
-    // Redirects to New Instant project project
       if(this.service.user_type === "Game Administrator"){
         this.router.navigate([]);
       }
