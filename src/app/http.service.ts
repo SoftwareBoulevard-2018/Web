d7bdc5d5bccb362925adc379173eed0fbe7ea7c7
Angel David Machado <amachadoa@unal.edu.co>
2018-06-02 04:47:23 -0500

    update parameter completed and update proyect completed too :)

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -19,21 +19,22 @@ import { InstantProject } from "./shared/instantProject";
 import { Question } from "./shared/question";
 import { Assignment } from "./shared/assignment";
 import { creationPuzzle } from './shared/creationPuzzle';
-import { invitations } from "./shared/invitations"
+import { invitations } from "./shared/invitations";
+import { GameAdmin } from "./shared/gameAdmin";
 @Injectable()
 
 export class HttpService {
 
   static httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type':  'application/json'
+      'Content-Type': 'application/json'
     })
   };
 
   constructor(public http: HttpClient) { }
 
-  static apiURL = 'http://35.196.111.251:3000';
-  //static apiURL = 'http://localhost:3000';
+  //static apiURL = 'http://35.196.111.251:3000';
+  static apiURL = 'http://localhost:3000';
   static usersURL = '/users';
   static usersURL2 = '/username';
   static usersURL3 = '/usersByRole';
@@ -56,8 +57,9 @@ export class HttpService {
   static instantProjecstURL = '/instantProjects';
   static assignmentsURL = '/assignments';
   static questionsURL = '/questions';
-  static invitationsURL='/invitations';
-  static gameAdministratorURL = '/gameAdministrator';
+  static invitationsURL = '/invitations';
+  static gameAdministratorURL = '/updateGA';
+
   // All services related to Users
   getAllUsers() {
     return this.http.get<User[]>(HttpService.apiURL + HttpService.usersURL);
@@ -85,6 +87,11 @@ export class HttpService {
       JSON.stringify({ role1: role }), HttpService.httpOptions);
   }
 
+  // All services related to game admin
+  updateGA(gameAdmin, userId) {
+    return this.http.put<Object>(HttpService.apiURL + HttpService.usersURL + '/' + userId,
+      JSON.stringify(gameAdmin), HttpService.httpOptions);
+  }
 
   // All services related to companies
   getAllCompanies() {
@@ -113,17 +120,17 @@ export class HttpService {
 
   // All services related to email
   read(idUsuario) {
-     return this.http.get<Email[]>(HttpService.apiURL + HttpService.emailURL + '/read/' + idUsuario);
+    return this.http.get<Email[]>(HttpService.apiURL + HttpService.emailURL + '/read/' + idUsuario);
   }
-  send(email: Email){
+  send(email: Email) {
     return this.http.post<Email>(HttpService.apiURL + HttpService.emailURL + '/send/',
       JSON.stringify(email), HttpService.httpOptions);
   }
   sent(idUsuario) {
-     return this.http.get<Email[]>(HttpService.apiURL + HttpService.emailURL + '/sent/' + idUsuario);
+    return this.http.get<Email[]>(HttpService.apiURL + HttpService.emailURL + '/sent/' + idUsuario);
   }
-  updateState(idEmail, email){
-    return this.http.put<Email>(HttpService.apiURL + HttpService.emailURL + '/updateState/'+idEmail,
+  updateState(idEmail, email) {
+    return this.http.put<Email>(HttpService.apiURL + HttpService.emailURL + '/updateState/' + idEmail,
       JSON.stringify(email), HttpService.httpOptions);
   }
   getTrainingAttemptsByState(state) {
@@ -144,7 +151,7 @@ export class HttpService {
   createDevelopingAttempt(developingAttempt: DevelopingAttempt) {
     return this.http.post<DevelopingAttempt[]>(HttpService.apiURL + HttpService.developingAttemptsURL,
       JSON.stringify(developingAttempt), HttpService.httpOptions);
-    }
+  }
   //All services related to records
   createRecord(record: Record) {
     return this.http.post<any>(HttpService.apiURL + HttpService.recordsURL,
@@ -161,7 +168,7 @@ export class HttpService {
   }
   getRecordsByFinishDateAndCompany(finishDate, company) {
     return this.http.post<Record>(HttpService.apiURL + HttpService.recordsURL + HttpService.getCurrentCompanyURL,
-      JSON.stringify({company: company , finishDate: finishDate}), HttpService.httpOptions);
+      JSON.stringify({ company: company, finishDate: finishDate }), HttpService.httpOptions);
   }
 
   //All services related to Puzzles
@@ -169,8 +176,8 @@ export class HttpService {
     return this.http.get<Puzzle[]>(HttpService.apiURL + HttpService.puzzleURL);
   }
 
-  createPuzzle(puzzle : creationPuzzle){
-	return this.http.post<creationPuzzle>(HttpService.apiURL + '/puzzles' + '/createPuzzle',
+  createPuzzle(puzzle: creationPuzzle) {
+    return this.http.post<creationPuzzle>(HttpService.apiURL + '/puzzles' + '/createPuzzle',
       JSON.stringify(puzzle), HttpService.httpOptions);
   }
 
@@ -181,19 +188,19 @@ export class HttpService {
   }
   getEstimationByPMAndProject(projectManagerUsername, projectName) {
     return this.http.post<Estimation[]>(HttpService.apiURL + HttpService.estimationURL + HttpService.getEstimationByPMAndProjectURL,
-      JSON.stringify({projectManagerUsername: projectManagerUsername , projectName: projectName}), HttpService.httpOptions);
+      JSON.stringify({ projectManagerUsername: projectManagerUsername, projectName: projectName }), HttpService.httpOptions);
   }
   getEstimationByProjectManagerUsernameAndState(projectManagerUsername, state) {
     return this.http.post<Estimation[]>(HttpService.apiURL + HttpService.estimationURL + HttpService.getEstimationsByPMAndStateURL,
-      JSON.stringify({projectManagerUsername: projectManagerUsername , state: state}), HttpService.httpOptions);
+      JSON.stringify({ projectManagerUsername: projectManagerUsername, state: state }), HttpService.httpOptions);
   }
 
   //All services related to BiddingProjects
   getAllBiddingProjects() {
-    return this.http.get<BiddingProject[]>(HttpService.apiURL + HttpService.getBiddingProjectURL+'/getBiddingProject/' );
+    return this.http.get<BiddingProject[]>(HttpService.apiURL + HttpService.getBiddingProjectURL + '/getBiddingProject/');
   }
   getBiddingProjectById(id: String) {
-    return this.http.get<BiddingProject>(HttpService.apiURL + HttpService.getBiddingProjectURL+ '/' + id);
+    return this.http.get<BiddingProject>(HttpService.apiURL + HttpService.getBiddingProjectURL + '/' + id);
   }
   createBiddingProject(biddingProject: BiddingProject) {
     return this.http.post<Id>(HttpService.apiURL + HttpService.getBiddingProjectURL + '/createBiddingProject/',
@@ -224,47 +231,47 @@ export class HttpService {
   }
   // All services related to Certification
 
-  getCertification(){
+  getCertification() {
     return this.http.get<Certification[]>(HttpService.apiURL + HttpService.certificationURL + '/getCertification/');
   }
-  createCertification(cert: Certification){
+  createCertification(cert: Certification) {
     return this.http.post<Certification>(HttpService.apiURL + HttpService.certificationURL + '/createCertification/',
       JSON.stringify(cert), HttpService.httpOptions);
   }
-  updateCertification(id, cert: Certification){
-    return this.http.put<Certification>(HttpService.apiURL + HttpService.certificationURL + '/updateCertification/' +id,
+  updateCertification(id, cert: Certification) {
+    return this.http.put<Certification>(HttpService.apiURL + HttpService.certificationURL + '/updateCertification/' + id,
       JSON.stringify(cert), HttpService.httpOptions);
   }
 
   // All services related to Questions
 
-  getQuestions(){
+  getQuestions() {
     return this.http.get<Question[]>(HttpService.apiURL + HttpService.questionsURL);
   }
-  createQuestion(question : Question){
-	return this.http.post<Question>(HttpService.apiURL + '/questions' + '/createQuestion',
+  createQuestion(question: Question) {
+    return this.http.post<Question>(HttpService.apiURL + '/questions' + '/createQuestion',
       JSON.stringify(question), HttpService.httpOptions);
   }
-  getQuestionsById(id: string){
+  getQuestionsById(id: string) {
     return this.http.get<Question>(HttpService.apiURL + HttpService.questionsURL + '/getQuestionById/' + id);
   }
-  updateQuestionById(question: Question, id: string){
+  updateQuestionById(question: Question, id: string) {
     return this.http.put<Question>(HttpService.apiURL + HttpService.questionsURL + '/updateQuestion/' + id,
       JSON.stringify(question), HttpService.httpOptions);
   }
 
   // All services related to assignment
-  getAssignment(){
+  getAssignment() {
     return this.http.get<Assignment[]>(HttpService.apiURL + HttpService.assignmentsURL + '/getAssignment/')
   }
-  createAssignment(ass: Assignment){
+  createAssignment(ass: Assignment) {
     return this.http.post<Assignment>(HttpService.apiURL + HttpService.assignmentsURL + '/createAssignment/',
       JSON.stringify(ass), HttpService.httpOptions);
   }
-  getAssignmentProjectById(id: string){
-    return this.http.get<Assignment>(HttpService.apiURL + HttpService.assignmentsURL+ '/' + id);
+  getAssignmentProjectById(id: string) {
+    return this.http.get<Assignment>(HttpService.apiURL + HttpService.assignmentsURL + '/' + id);
   }
-  updateAssignment(id, ass: Assignment){
+  updateAssignment(id, ass: Assignment) {
     return this.http.put<Assignment>(HttpService.apiURL + HttpService.assignmentsURL + '/updateAssignment/' + id,
       JSON.stringify(ass), HttpService.httpOptions);
   }
@@ -274,18 +281,18 @@ export class HttpService {
     return this.http.get<invitations[]>(HttpService.apiURL + HttpService.invitationsURL);
   }
   createinvitations(invitation: invitations) {
-    return this.http.post<invitations>(HttpService.apiURL + HttpService.invitationsURL ,
+    return this.http.post<invitations>(HttpService.apiURL + HttpService.invitationsURL,
       JSON.stringify(invitation), HttpService.httpOptions);
   }
-  getinvitationsByUserAndCompany(user, company){
-    return this.http.post<invitations>(HttpService.apiURL + HttpService.certificationURL + '/getCurrentInvitationCom/' ,
-      JSON.stringify(user,company), HttpService.httpOptions);
+  getinvitationsByUserAndCompany(user, company) {
+    return this.http.post<invitations>(HttpService.apiURL + HttpService.certificationURL + '/getCurrentInvitationCom/',
+      JSON.stringify(user, company), HttpService.httpOptions);
   }
   getInvitationByUserAndState(id, state) {
     return this.http.post<invitations[]>(HttpService.apiURL + HttpService.invitationsURL + '/getCurrentInvitation',
-      JSON.stringify({ user: id, state: state}), HttpService.httpOptions);
+      JSON.stringify({ user: id, state: state }), HttpService.httpOptions);
   }
-  updateInvitation(invitation, id: String){
+  updateInvitation(invitation, id: String) {
     return this.http.put<invitations>(HttpService.apiURL + HttpService.invitationsURL + '/' + id,
       JSON.stringify(invitation), HttpService.httpOptions);
   }
